c9bc8a0d45522e73694a3d7d2f52e7b6ea4dcc7c
Mrutyunjaya Samantray <s.mrutyunjaya94@gmail.com>
2022-06-27 12:02:26 +0530

    Cart me class added with basic logic..

---
+++ src/main/java/com/qa/opencart/pages/CartMe.java
@@ -0,0 +1,12 @@
+package com.qa.opencart.pages;
+
+public class CartMe {
+
+	public static void main(String[] args) {
+		int num = 5;
+		int digit = 10;
+		System.out.println("Total " + num + digit);
+
+	}
+
+}
